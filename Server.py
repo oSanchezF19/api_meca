a63facf0c2ba00abd8447e58fb034ac6b43b11c0
Oscar Sanchez <oscar@microbit.com>
2020-06-18 15:53:26 -0500

    Edit server.py add Subsitemas.py

--- Server.py
+++ Server.py
@@ -1,29 +1,69 @@
-from flask import Flask
+'''
+
+Aplicacion api para probar los metodos GET, POST, PUT Y DELETED
+
+'''
+from flask import Flask, jsonify, request
+from Subsistemas import Subsistemas
 
 app = Flask(__name__)
 
+@app.route('/')
+def page():
+    return "Soldadora de friccion por aglomeracion" 
 
-@app.route("/")
-def main():
-    return {
-        "Soldadora de friccion por aglomeración": {
-            "Eje horizontal": {
-                "Velocidad": 500,
-                "Distancia": 1500,
-                "Paso": 25,
-            },
-            "Eje Vertical": {
-                "Velocidad": 50,
-                "Altura": 10,
-                "Paso": 25,
-            },
-            "Disco soldadura": {
-                "Velocidad": 18000,
-                "Diametro": 200,
-                "Geometria": "Tipo 1",
-                "Ancho": 20,
-            }
-        }}
+@app.route('/Subsistemas')
+def getSubsistemas():
+    return jsonify({"mensaje": "Variables de control", "Subsistemas": Subsistemas})
+
+@app.route('/Subsistemas/<string:subsistema_Nombre>')
+def getsubsistema(subsistema_Nombre):
+    SubsistemasFound = [subsistema for subsistema in Subsistemas if subsistema['Nombre'] == subsistema_Nombre]
+    if (len(SubsistemasFound) > 0):
+        return jsonify({"subsistema":SubsistemasFound[0]})
+    return jsonify({"mensaje":"error 404 subsistema no encontrado"})
+
+@app.route('/Subsistemas', methods=['POST'])
+def addsubsistema():
+    new_subsistema ={
+        "Nombre": request.json['Nombre'],
+        "Velocidad": request.json['Velocidad'],
+        "Distancia": request.json['Distancia'],
+        "Paso": request.json['Paso'],
+        "Diametro": request.json['Diametro'],
+        "Geometria": request.json['Geometria'],
+        "Ancho": request.json['Ancho']
+    }
+    Subsistemas.append(new_subsistema)
+    return jsonify({"mensaje:":"Subsistemas added Succesfully", "Subsistemas": Subsistemas})
+
+@app.route('/Subsistemas/<string:subsistema_Nombre>', methods=['PUT'])
+def editsubsistema(subsistema_Nombre):
+    subsistemaFound= [subsistema for subsistema in Subsistemas if subsistema['Nombre'] == subsistema_Nombre]
+    if (len(subsistemaFound) > 0):
+        subsistemaFound[0]['Nombre'] = request.json['Nombre']
+        subsistemaFound[0]['Velocidad'] = request.json['Velocidad']
+        subsistemaFound[0]['Distancia'] = request.json['Distancia']
+        subsistemaFound[0]['Paso'] = request.json['Paso']
+        subsistemaFound[0]['Diametro'] = request.json['Diametro']
+        subsistemaFound[0]['Geometria'] = request.json['Geometria']
+        subsistemaFound[0]['Ancho'] = request.json['Ancho']
+        return jsonify({
+        "mensaje": "subsistema Update",
+        "subsistema":subsistemaFound[0]
+        })
+    return jsonify({"mesage":"error 404 subsistema no encontrado"})
+
+@app.route('/Subsistemas/<string:subsistema_Nombre>', methods=['DELETE'])
+def delectsubsistema(subsistema_Nombre):
+    subsistemaFound= [subsistema for subsistema in Subsistemas if subsistema['Nombre'] == subsistema_Nombre]
+    if (len(subsistemaFound) > 0):
+        Subsistemas.remove(subsistemaFound[0])
+        return jsonify({
+            "mensaje": "subsistema eliminado",
+            "Subsistemas": Subsistemas
+        })
+    return jsonify({"mesage":"error 404 subsistema no encontrado"})
 
 
 if __name__ == "__main__":
---
+++ Subsistemas.py
@@ -0,0 +1,28 @@
+Subsistemas = [
+    {"Nombre": "Eje_horizontal",
+        "Velociadad": 800, 
+        "Distancia": 4,
+        "Paso": 4,
+        "Diametro": "null",
+        "Geometria": "null",
+        "Ancho": "null",
+    },
+
+    {"Nombre": "Eje_vertical",
+        "Velociadad": 50, 
+        "Distancia": 10,
+        "Paso": 25,
+        "Diametro": "null",
+        "Geometria": "null",
+        "Ancho": "null",
+    },
+    {"Nombre": "Disco_de_Soldadura",
+        "Velociadad": 18000, 
+        "Distancia": "null",
+        "Paso": "null",
+        "Diametro": 200,
+        "Geometria": "Tipo 1",
+        "Ancho": 20,
+    }
+
+]
